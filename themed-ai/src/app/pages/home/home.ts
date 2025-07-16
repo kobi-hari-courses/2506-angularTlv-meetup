f0a971da1c2d043f6b96e32b62b6d7d3f32a5d7a
Kobi Hari <kobi2294@yahoo.com>
2025-07-16 11:13:19 +0300

    demo start point

--- themed-ai/src/app/pages/home/home.ts
+++ themed-ai/src/app/pages/home/home.ts
@@ -6,26 +6,27 @@ import { Component } from '@angular/core';
   standalone: true,
 })
 export class HomeComponent {
-bars = [
-  this.getBarItem(0, 413, 'Support'),
-  this.getBarItem(1, 240, 'Sales'),
-  this.getBarItem(2, 520, 'General'),
-  this.getBarItem(3, 165, 'Tech')
-];
+  readonly #colors = ['#5aabf8', '#2a92f4', '#2196f3', '#1a7ace'];
 
-getBarItem(index: number, value: number, label: string) {
-  const height = value * 0.13;  
-
-  return {
-    x: index * 70 + 20, // Adjust x position based on index
-    y: -height + 90, // Adjust y position based on height
-    width: 50, 
-    height: height, 
-    fill: `var(--p-0${index + 3})`, // Use a dynamic fill color
-    value: value, 
-    label: label
-  };
-}
+  bars = [
+    this.getBarItem(0, 413, 'Support'),
+    this.getBarItem(1, 240, 'Sales'),
+    this.getBarItem(2, 520, 'General'),
+    this.getBarItem(3, 165, 'Tech'),
+  ];
 
 
+  getBarItem(index: number, value: number, label: string) {
+    const height = value * 0.13;
+
+    return {
+      x: index * 70 + 20, // Adjust x position based on index
+      y: -height + 90, // Adjust y position based on height
+      width: 50,
+      height: height,
+      fill: this.#colors[index], // Use a dynamic fill color
+      value: value,
+      label: label,
+    };
+  }
 }
