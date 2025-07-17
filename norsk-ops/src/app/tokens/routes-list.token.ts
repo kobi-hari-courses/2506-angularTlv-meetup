a51d4847355371a841f88f264c2ef57321ee9914
Kobi Hari <kobi2294@yahoo.com>
2025-07-17 18:53:01 +0300

    removed old project

--- norsk-ops/src/app/tokens/routes-list.token.ts
+++
@@ -1,5 +0,0 @@
-import { InjectionToken } from "@angular/core";
-import { Routes } from "@angular/router";
-import { PageRoute } from "../app.routes";
-
-export const ROUTES_LIST = new InjectionToken<PageRoute[]>('ROUTES_LIST');
